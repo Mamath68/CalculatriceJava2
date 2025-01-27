0b64c95d182d0e45d7f8025d00b57c86d039760f
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-01-27 16:27:33 +0100

    ajout builder et personalisation EType

--- src/EType.java
+++ src/EType.java
@@ -1,3 +1,17 @@
 public enum EType {
-    VALEURS, OPERATEURS
+    VALEURS("Valeurs"), OPERATEURS("Operateurs");
+
+    private final String type;
+
+    EType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String toString() {
+        return this.getType();
+    }
 }
